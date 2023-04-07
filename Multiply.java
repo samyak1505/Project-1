a24c42306924251ab027538479c8cf1db3567dc6
Samyak Bharat Dahat <samygamer5589@gmail.com>
2023-04-07 14:36:28 +0530

    added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,11 @@
+public class Multiply{
+    public static void main(String args[])
+    {
+        int a = 4;
+        int b = 5;
+        System.out.println(
+            "The multiplication result is " + 
+            (a * b)
+        );
+    }
+}
